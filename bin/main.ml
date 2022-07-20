9d2f0125e6551d04661f7039583df2fd2e285290
Yutaro Sakamoto <yutaro-sakamoto@yutaro-sakamoto.com>
2022-07-20 23:35:55 +0900

    Looping over strings; Approach 3

--- bin/main.ml
+++ bin/main.ml
@@ -2,15 +2,14 @@ open Printf
 
 let read_whole_chan chan =
   let buf = Buffer.create 4096 in
-  try
-    while true do
-      let line = input_line chan in
+  let rec loop () =
+    let line = input_line chan in
       Buffer.add_string buf line;
-      Buffer.add_char buf '\n'
-    done;
-    assert false (*Never executed*)
-  with
-    End_of_file -> Buffer.contents buf
+      Buffer.add_char buf '\n';
+      loop ()
+  in
+    try loop () with
+      End_of_file -> Buffer.contents buf
 
 let read_whole_file filename =
   let chan = open_in filename in
