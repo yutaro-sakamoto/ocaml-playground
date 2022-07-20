b018d9b2f62351dd560cdf595753fa012f945c6f
Yutaro Sakamoto <yutaro-sakamoto@yutaro-sakamoto.com>
2022-07-21 00:04:05 +0900

    Modules; basic usage

--- bin/main.ml
+++ bin/main.ml
@@ -1,20 +1,7 @@
+open Amodule
 open Printf
 
-let read_whole_chan chan =
-  let buf = Buffer.create 4096 in
-  let rec loop () =
-    let line = input_line chan in
-      Buffer.add_string buf line;
-      Buffer.add_char buf '\n';
-      loop ()
-  in
-    try loop () with
-      End_of_file -> Buffer.contents buf
-
-let read_whole_file filename =
-  let chan = open_in filename in
-    read_whole_chan chan
+let data = ["a"; "beautiful"; "day"]
 let () =
-  let filename = Sys.argv.(1) in
-  let str = read_whole_file filename in
-    printf "I read %d characters from %s\n" (String.length str) filename
+  hello ();
+  List.iter (printf "%s\n") data
